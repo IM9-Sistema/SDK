144317039bfa3f32a7c2e6793518ca94312a434a
João Bernardi (Mobile) <joao_bernardi@outlook.com>
2024-08-20 16:55:14 -0300

    `Version` - Bump

--- standards/utils/singleton.py
+++ standards/utils/singleton.py
@@ -5,7 +5,7 @@ class SingletonMeta(type):
 
     def __new__(cls, *args, **kwargs):
         if not cls in cls._instances:
-            cls._instances[cls] = super(SingletonMeta, cls).__call__(*args, **kwargs)
+            cls._instances[cls] = super(SingletonMeta, cls).__new__(*args, **kwargs)
         return cls._instances[cls]
 
 class Singleton(object, metaclass=SingletonMeta):
