63b2368314655f57199e945181ad8bb1c907418e
João Bernardi (Mobile) <joao_bernardi@outlook.com>
2024-08-20 16:58:24 -0300

    `Fix` - Metaclass typo

--- standards/utils/singleton.py
+++ standards/utils/singleton.py
@@ -3,9 +3,9 @@ from ..exceptions import NotInstancedException
 class SingletonMeta(type):
     _instances = {}
 
-    def __new__(cls, *args, **kwargs):
+    def __call__(cls, *args, **kwargs):
         if not cls in cls._instances:
-            cls._instances[cls] = super(SingletonMeta, cls).__new__(*args, **kwargs)
+            cls._instances[cls] = super(SingletonMeta, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
 
 class Singleton(object, metaclass=SingletonMeta):
