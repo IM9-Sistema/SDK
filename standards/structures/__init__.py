82b6ad00000b0f261ba3bd8cf614bea5dc839b59
João Bernardi (Mobile) <joao_bernardi@outlook.com>
2024-08-21 16:40:12 -0300

    `Positions` - Add `BatteryData` and `ExtraInfo` to exports

--- standards/structures/__init__.py
+++ standards/structures/__init__.py
@@ -1,7 +1,7 @@
 from .base import BaseObject
 from .equipament import Equipament, EquipamentEventType, EquipamentReference, PrimitiveEquipament, PrimitiveEquipamentReference
 from .event import Event, EventType, Issuer
-from .position import Position, PrimitivePosition
+from .position import Position, PrimitivePosition, BatteryData, ExtraInfo
 from .generics import GenericReference, Primitive, IDReference, HostAddress, RabbitMQAddress
 from .config import StandardConfig, SingletonConfig
 from .auth import PasswordUsername
