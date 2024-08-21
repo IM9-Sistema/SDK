c4aa9c6b297cb71e193e85aeb7c2e5d38aef5026
João Bernardi (Mobile) <joao_bernardi@outlook.com>
2024-08-21 14:19:47 -0300

    `Singleton` - Fix get method

--- standards/utils/singleton.py
+++ standards/utils/singleton.py
@@ -5,14 +5,17 @@ from pydantic._internal._model_construction import ModelMetaclass
 class SingletonMeta(ModelMetaclass):
     _instances = {}
 
-    def __init__(cls, *args, **kwargs):
+    def __call__(cls, *args, **kwargs):
         if not cls in cls._instances:
-            cls._instances[cls] = super(SingletonMeta, cls).__init__(*args, **kwargs)
+            cls._instances[cls] = super(SingletonMeta, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
+
+
 
 class Singleton(metaclass=SingletonMeta):
     @classmethod
     def get(cls):
+        print(cls._instances, cls)
         if cls in cls._instances:
             return cls._instances[cls]
         raise NotInstancedException()
