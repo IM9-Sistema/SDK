6c2780318784479a45a764c130a09509f57103fb
João Bernardi (Desktop) <joao_bernardi@outlook.com>
2024-08-27 12:46:30 -0300

    `Version` - Bump

--- standards/structures/__init__.py
+++ standards/structures/__init__.py
@@ -2,6 +2,6 @@ from .base import BaseObject
 from .equipament import Equipament, EquipamentEventType, EquipamentReference, PrimitiveEquipament, PrimitiveEquipamentReference, VehicleState
 from .event import Event, EventType, Issuer
 from .position import Position, PrimitivePosition, BatteryData, ExtraInfo
-from .generics import GenericReference, Primitive, IDReference, HostAddress, RabbitMQAddress
-from .config import StandardConfig, SingletonConfig
+from .generics import GenericReference, Primitive, IDReference, HostAddress, RabbitMQAddress, RabbitMQPool
+from .config import StandardConfig, SingletonConfig, MSSQLConfig
 from .auth import PasswordUsername
