855145e09449e9d580762a4c484028f1a9d939de
João Bernardi (Desktop) <joao_bernardi@outlook.com>
2024-08-27 12:41:37 -0300

    `Geo` - Add `get_reverse_address()`

--- standards/utils/__init__.py
+++ standards/utils/__init__.py
@@ -1,2 +1,3 @@
 from .references import reference_link
 from .singleton import SingletonMeta, Singleton
+from .geo import get_reverse_address
---
+++ standards/utils/geo.py
@@ -0,0 +1,17 @@
+import requests
+from os import environ
+
+
+def get_reverse_address(lat: float, lon: float) -> str:
+    request = requests.get(environ.get('NOMINATIM_HOST', 'http://10.15.1.102:8080/')+'/reverse', params={
+        'lat': lat, 'lon': lon, 'format': 'jsonv2'
+    })
+
+    if request.status_code == 500:
+        return get_reverse_address(lat, lon)
+
+    if request.status_code == 404:
+        return environ.get('NOT_FOUND_ADDRESS', 'Endereço não encontrado')
+
+    data = request.json()
+    return data['display_name']
