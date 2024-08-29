c588e1323b2036ed290c445d3ddc068cb78915fc
João Bernardi (Desktop) <joao_bernardi@outlook.com>
2024-08-29 14:59:34 -0300

    `Version` - Bump

--- standards/utils/geo.py
+++ standards/utils/geo.py
@@ -3,9 +3,12 @@ from os import environ
 
 
 def get_reverse_address(lat: float, lon: float) -> str:
-    request = requests.get(environ.get('NOMINATIM_HOST', 'http://10.15.1.102:8080/')+'/reverse', params={
-        'lat': lat, 'lon': lon, 'format': 'jsonv2'
-    })
+    try:
+        request = requests.get(environ.get('NOMINATIM_HOST', 'http://10.15.1.102:8080/')+'/reverse', params={
+            'lat': lat, 'lon': lon, 'format': 'jsonv2'
+        })
+    except:
+        return get_reverse_address(lat, lon)
 
     if request.status_code == 500:
         return get_reverse_address(lat, lon)
