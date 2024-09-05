d8fa5edc90ff4fcb2e9dcd0b454ca74517172cc6
João Bernardi (Desktop) <joao_bernardi@outlook.com>
2024-09-05 04:35:18 -0300

    `Fix` - Circular imports

--- standards/structures/risk_area.py
+++ standards/structures/risk_area.py
@@ -14,18 +14,19 @@ class RiskAreaReference(GenericReference):
 class RiskArea(BaseObject):
     id: int
     name: str
-    points: list[list[float, float]]
-
-    @classmethod
-    def from_geojson(cls, id: int, name: str, geojson: dict):
-        polygon: Polygon
-        if not isinstance(polygon := shape_from_geojson(geojson), Polygon):
-            raise ShapeException(f'Expecting Polygon got {type(polygon)}')
-        x, y = polygon.coords.xy
-        points = [[a, b] for a,b in zip(x,y)]
-
-        return cls(id=id, name=name, points=points)
-
-    @property
-    def polygon(self) -> Polygon:
-        return Polygon(self.points)
+    # TODO: Implement
+    # points: list[list[float, float]]
+    #
+    # @classmethod
+    # def from_geojson(cls, id: int, name: str, geojson: dict):
+    #     polygon: Polygon
+    #     if not isinstance(polygon := shape_from_geojson(geojson), Polygon):
+    #         raise ShapeException(f'Expecting Polygon got {type(polygon)}')
+    #     x, y = polygon.coords.xy.toList()
+    #     points = [[a, b] for a,b in zip(x,y)]
+    #
+    #     return cls(id=id, name=name, points=points)
+    #
+    # @property
+    # def polygon(self) -> Polygon:
+    #     return Polygon(self.points)
