dc1ba1b9843300cbeddc56cdbe1261faa84402b7
jaobernardi <joao_bernardi@outlook.com>
2024-11-27 01:03:14 -0300

    `Queues` - Update config to match new options

--- standards/structures/generics.py
+++ standards/structures/generics.py
@@ -1,4 +1,5 @@
 from .base import BaseObject
+from pydantic import Field
 
 
 class GenericReference(BaseObject): pass
@@ -20,6 +21,9 @@ class GenericConnectionPool(BaseObject):
 class RabbitMQAddress(HostAddress):
     queue: str
     durable: bool
+    stream: bool = Field(default_factory=lambda: False)
+    max_size: int|None = Field(default_factory=lambda: None)
+    ttl: str | None = Field(default_factory=lambda: None)
 
 class RabbitMQPool(RabbitMQAddress):
     pool: GenericConnectionPool
